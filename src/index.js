28d156bcfc019ec59888d08565bcdfd47cce7aff
Natali <natalic1008.nm@gmail.com>
2023-06-09 13:41:02 -0500

    cambios 2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+
+const textarea =document.getElementsByName('user-input')[0];
+textarea.addEventListener('input',() =>{
+    const value = textarea.value
+    document.getElementsByClassName('palabras')[0].innerHTML="Palabras: "+analyzer.getWordCount(value);
+    document.getElementsByClassName('caracteres')[0].innerHTML="Caracteres: "+analyzer.getCharacterCount(value);
+ } )
+
+const resetButton = document.getElementById("reset-button");
+resetButton.addEventListener('click',resetAll);
+
+function resetAll(){
+    document.getElementsByName('user-input')[0].value=" ";
+    document.getElementsByClassName('palabras')[0].value= " "
+}
