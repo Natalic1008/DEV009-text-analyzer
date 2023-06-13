80046a0d36646efc560be69b18d5766f8048c2c7
Natali <natalic1008.nm@gmail.com>
2023-06-12 21:46:03 -0500

    Funciones de contar palabras sin espacios, longitud media..ok

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,20 +1,35 @@
 const analyzer = {  
   getWordCount: (text) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
-    return text.trim().split(" ").length;
+    return text.trim().split(' ').length;
   },
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
-  return text.length;
+    return text.length;
   },
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    const str = text.trim().replace(/[\s,.#!$%&;:{}=\-_`~()""¨...]+/g,'');
+    return str.length;
   },
   getAverageWordLength: (text) => {    
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    const totalCharacters=text.trim().replace(/\s+/g,'').length;
+    const numWords=text.trim().split(/\s+/).length;
+    return parseFloat((totalCharacters/numWords).toFixed(2));
   },
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+  
+    const countNumber = 0;
+    const results = results.split(' ');
+
+    for (let i=0; i<(results.length);i++){
+      if(isNaN(results[i])){
+        countNumber=countNumber+1;
+      }
+      return countNumber;
+    }
   },
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
